363d6d8cc76157804c8d386588ca45c40f16a8a3
Ruan-Portella <ruanmorales29@gmail.br>
2022-11-24 19:12:54 -0300

    exercicio_2 concluido

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios_2/scripts.js
@@ -0,0 +1,15 @@
+// 1. Crie uma constante chamada base e atribua a ela o valor 5.
+// 2. Crie uma constante uma chamada heigth e atribua a ela o valor 8.
+// 3. Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+// 4. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log(area);
+
+const perimeter = base + heigth;
+
+console.log(perimeter);
+
