bd7fbc8a23b245ac68dad1af6485afecbdfc241e
Ruan-Portella <ruanmorales29@gmail.br>
2022-12-05 20:53:10 -0300

    Feat: Resolvendo Exercicio 04

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-Logica-de-progamacao-e-algoritmos/exercicios_4/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-Logica-de-progamacao-e-algoritmos/exercicios_4/scripts.js
@@ -1,63 +1,17 @@
-// 1. Percorra o array imprimindo todos os valores nele contidos com a função console.log();
-// 2. Some todos os valores contidos no array e imprima o resultado;
-// 3. Calcule e imprima a média aritmética dos valores contidos no array;
-// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
-// 5. Utilizando for, descubra qual o maior valor contido no array e imprima-o;
-// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
-// 8. Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
-// 9. Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+// 1. Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
 
+let maiorPrimo = 0;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
-let soma = 0
-let maior = numbers[0]
+    for (let numero = 2; numero <= 50; numero += 1) {
+        let ePrimo = true;
+        for (let divisor = 2; divisor < numero; divisor += 1) {
+            if (numero % divisor === 0) {
+                ePrimo = false;
+            }
+        }
+        if (ePrimo) {
+            maiorPrimo = numero
+        }
+    }
 
-//    for (let number of numbers) {
-//         console.log(number)
-        
-//    }
-
-    // for (let index = 0; index < numbers.length; index +=1) {
-    //         soma += numbers[index];
-    //         console.log(soma);
-    // }
-
-    // for (let index = 0; index < numbers.length; index +=1) {
-    //            soma += numbers[index];
-    // }
-
-    // let media = soma / numbers.length
-    // console.log(media)
-
-    // if (soma > 20) {
-    //     console.log('valor maior que 20');
-    // } else if (soma <= 20) {
-    //     console.log('valor menor que 20');
-    // }
-
-    // for (let index = 0; index < numbers.length; index +=1) {
-    //         if(numbers[index] > maior) {
-    //             maior = numbers[index];
-    //         }
-    // } console.log(maior)
-
-    //     for (let index = 0; index < numbers.length; index +=1) {
-    //         if(numbers[index] %2 !== 0) {
-    //             maior += 1;
-    //         }
-    // } if (maior === 0) {
-    //     console.log('nenhum valor')
-    // } else {
-    //     console.log(maior)
-    // }
-
-// let numbers_2 = []
-
-//     for (let index = 1; index <= 25; index += 1) {
-//         numbers_2.push(index);
-//     } console.log(numbers_2(index) / 2);
-
- 
-
-
-
+    console.log(maiorPrimo);
