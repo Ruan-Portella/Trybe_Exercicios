e51e8375422b9a4099e90dfceead9981ccfe15fe
Ruan-Portella <ruanmorales29@gmail.br>
2022-12-13 12:35:32 -0300

    Feat: Resolvendo Exercicio_2

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios_2/scripts.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios_2/scripts.js
@@ -1,9 +1,25 @@
-// 1. Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
 
-    for (let index = 0; index < groceryList.length; index += 1) {
-        console.log(groceryList[index]);
-    }
+ for (const name in names) {
+    console.log('Olá ' + names[name]);
+ }
 
+//  Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto
 
+ let car = {
+   model: 'A3 Sedan',
+   manufacturer: 'Audi',
+   year: 2020
+ };
+
+ for (const key in car) {
+    console.log(`${key} ${car[key]}`);
+    
+ }
+ 
