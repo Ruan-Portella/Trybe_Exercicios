98c8554d60accaead3360a409c1d4969da98a609
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-20 14:23:16 -0300

    Feat: Adicionando Exercicio do dia

---
+++ fundamentos/secao-07-introducao-a-javascriptes6/dia-02-primeiros-passos-em-jest/exercicios_2/exercises.js
@@ -0,0 +1,71 @@
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  } 
+  return newArr;
+} console.log(myRemove([1,2,3,4], 3));
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+const mapString = (objectMap, string) => {
+  const splitString = string.split('');
+  const mappedArray = [];
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+    const mappedValue = objectMap[character];
+    
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
+    }
+  }
+  return mappedArray.join('');
+}
+const encode = (string) => {
+  const map = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return mapString(map, string);
+}
+const decode = (string) => {
+  const map = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return mapString(map, string);
+}
+
+const techList = (arrayTechnologies, name) => {
+  if (arrayTechnologies.length === 0) return 'Vazio!';
+
+  const sortedArray = arrayTechnologies.sort();
+  const technologyList = [];
+
+  for (let index = 0; index < sortedArray.length; index += 1) {
+    technologyList.push({
+      tech: sortedArray[index],
+      name,
+    });
+  }
+
+  return technologyList;
+};
+
+  module.exports = {myRemove, myFizzBuzz, mapString, encode, decode, techList};
