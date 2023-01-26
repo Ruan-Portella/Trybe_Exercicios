2a66a9af67bacd710816e9c9f5b1e495384d3e8b
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-26 15:10:13 -0300

    Feat: Adicionando Exercicicio de Fixação

---
+++ fundamentos/secao-08-higherorder-javascriptes6/dia-02-sort-e-map/exercicios_1/script.js
@@ -0,0 +1,13 @@
+// Utilize o sort para ordenar o array pela idade das pessoas em ordem crescente.
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age > b.age ? 1 : -1)
+
+console.log(people);
