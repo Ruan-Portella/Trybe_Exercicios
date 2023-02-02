719f22871fc2b289c812b77b31dc662dfbe66a32
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-02 17:38:40 -0300

    Feat: Resolvendo Exercicio Do Dia

--- fundamentos/secao-08-higherorder-javascriptes6/dia-05-Array-Default-Objecte-e-Parameters/exercicios_2/script.js
+++ fundamentos/secao-08-higherorder-javascriptes6/dia-05-Array-Default-Objecte-e-Parameters/exercicios_2/script.js
@@ -1,18 +1,70 @@
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+// Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando apenas 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5].
 
-HREF_LINK.addEventListener("click", (event)=>{
-    event.preventDefault();
-})
+const myList = [5, 2, 3];
 
-INPUT_CHECKBOX.addEventListener("click", (event)=>{
-    event.preventDefault();
-})
+const [five, two, three] = myList;
 
-INPUT_TEXT.addEventListener("keypress", (event)=>{
-    const evento = event.key
-    if (evento != 'a'){
-    event.preventDefault();
+console.log(three, two, five);
+
+// Exercício 2
+// Suponha que você esteja trabalhando em projeto de um site de carros, onde cada carro é representado dentro de um array. Então sua liderança pede que seja mudado o formato de array para objeto. Para isso, crie uma função chamada toObject que, dada uma lista, retorna um objeto representando o carro:
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([name, brand, year]) => ({ name, brand, year });
+console.log(toObject(palio));
+
+    
+
+// Exercício 3
+// Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento. Um parâmetro adicional pode ser passado para alterar o cumprimento utilizado:
+
+const greet = (user, mensage = 'Hi') => `${mensage} ${user}` 
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
+
+// Exercício 4
+// Escreva uma função getLastName que receba como parâmetro um objeto contendo informações de uma pessoa. Essa função deve retornar a propriedade lastName para o objeto passado, porém, caso o objeto não tenha essa propriedade, a função deve retornar a mensagem lastName não preenchido.
+
+const student1 = {
+  name: `Claudia`,
+  lastName: `Farias`,
+  age: 23,
 }
-})
+
+const student2 = {
+  name: `Vitor`,
+  age: 20,
+}
+
+// escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
+
+const getLastName = (obj) => {
+    const { lastName = 'Não Preenchido'} = obj
+    return lastName;
+}
+
+
+
+console.log(getLastName(student1));
+console.log(getLastName(student2));
+
+// Exercício 5
+// Usando array destructuring, armazene cada nome presente na variável moreStudents em variáveis separadas:
+
+const moreStudents = [
+    'Chris', 
+    ['Ahmad', 'Antigoni'], 
+    ['Toby', 'Sam']
+  ];
+
+  // Escreva seu código aqui
+  const [ studentx, [ student2x, student3 ], [student4, student5 ] ] = moreStudents;
+
+
+console.log(studentx, student2x, student3, student4, student5);
