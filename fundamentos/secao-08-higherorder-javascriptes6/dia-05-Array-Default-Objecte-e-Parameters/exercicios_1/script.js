055f4a9dfa9dcb9f471e2840a791c2a836598bf5
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-02 17:02:55 -0300

    Feat: Resolvendo Exercicio De Fixação

--- fundamentos/secao-08-higherorder-javascriptes6/dia-05-Array-Default-Objecte-e-Parameters/exercicios_1/script.js
+++ fundamentos/secao-08-higherorder-javascriptes6/dia-05-Array-Default-Objecte-e-Parameters/exercicios_1/script.js
@@ -33,3 +33,40 @@ console.log(numerosPares); // [6, 8, 10, 12];
 [,,, ...numerosPares] = numerosPares;
 
 console.log(numerosPares);
+
+//
+
+// o jeito que está, quando person é passado para a função getNationality, o retorno é João is undefined. Ajuste a função getNationality para que a chamada getNationality(person) retorne João is Brazilian.
+
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
+//
+
+// Agora é hora de praticar: altere a função getPosition utilizando a property shorthand.
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
+
+// escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1, caso não seja passado nenhum valor como segundo parâmetro.
+
+const multiply = (number, value = 1) => number + value;
+
+console.log(multiply(8));
