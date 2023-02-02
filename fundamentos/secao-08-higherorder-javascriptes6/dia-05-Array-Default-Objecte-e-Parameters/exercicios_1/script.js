937fd5b3b6332319dfccffe583ff5264369e7471
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-02 16:50:27 -0300

    Feat: Resolvendo Exercicio De Fixação

---
+++ fundamentos/secao-08-higherorder-javascriptes6/dia-05-Array-Default-Objecte-e-Parameters/exercicios_1/script.js
@@ -0,0 +1,35 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]) // 54
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [firstNumber, secondNumber, ThirdNumber] = primeNumbers;
+
+sum(firstNumber, ThirdNumber);
+
+// 
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = [bebida, comida, animal];
+console.log(comida, animal, bebida)
+//
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares);
