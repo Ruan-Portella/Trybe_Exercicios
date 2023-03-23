289025433f2d5bb05c2f6f5a3da480868ff951a6
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-23 13:21:02 -0300

    Feat: Removendo arquivo de Teste, Adicionando Seção 05 completa

--- teste/testad/src/__mocks__/localStorage.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-  email: 'test1@trybe.com',
-  text: 'Avaliação do produto 1',
-  rating: '5',
-}, {
-  email: 'test1@trybe.com',
-  text: 'Avaliação do produto 2',
-  rating: '4',
-  }];
