7b3c7c7108e8f67fc58b21cd5659d482db45815c
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-29 18:33:26 -0300

    Feat: Exercicio de Fixação Resolvido

---
+++ front-end/secao-06-testes-automatizados-com-react-testing-library/dia-03-rtl-testanto-react-router/testrouter/src/App.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App, { About } from './App';
+
+describe('teste da aplicação toda', () => {
+  it('deve renderizar o componente App', () => {
+    renderWithRouter(<App />);
+
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it('deve renderizar o componente Sobre', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/pagina/que-nao-existe/');
+    })
+
+    const notFoundTitle = screen.getByRole('heading',
+      { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+  it('deve renderizar o componente About (apenas componente)', () => {
+    renderWithRouter(<About />);
+  
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+});
