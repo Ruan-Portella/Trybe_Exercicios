ebfe5a43b95b7e797d5727c97d987b557ef275e4
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-11 15:14:59 -0300

    Feat: Cria a migration de Users

---
+++ back-end/secao-06-orm-e-autenticacao/dia-03-associations-N-N-e-Transactions/books-api-sequelize/src/migrations/20230711173947-create-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'first_name',
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'last_name',
+      },
+      age: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
